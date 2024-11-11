8e3fa7f384b4a7384b7914dfaa4f60f05d8c92db
SairamGolla918 <gollasairam918@gmail.com>
2024-11-11 14:25:46 +0530

    UPDTAED

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,14 +27,48 @@
 
 
 
-let someNum = 33
+// let someNum = 33
 
-let stringNum = String(someNum)
+// let stringNum = String(someNum)
 
-console.log(stringNum)
+// console.log(stringNum)
 
-console.log(typeof stringNum);
+// console.log(typeof stringNum);
 
+
+// --------------Operations------------------
+
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+
+// let str1 = "Saiee"
+// let str2 = "Yadav"
+
+// console.log(str1+str2)
+
+// console.log("1"+2)
+
+// console.log(1+1+"2")
+
+let num1 , num2 , num3
+
+num1 = num2 = num3  = 2+2
+
+let gameCounter = 100;
+gameCounter++
+
+console.log(gameCounter)
 
 
 
