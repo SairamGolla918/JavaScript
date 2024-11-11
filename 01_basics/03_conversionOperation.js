7da6ec5703d694197db53ba902c14082e277f6d9
SairamGolla918 <gollasairam918@gmail.com>
2024-11-11 12:19:27 +0530

    updated 03-conversion of types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -10,11 +10,21 @@
 // console.log(valueInNumber);
 
 
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
 // let isLoggedIn = ""
 
 // let boolIsLoggedIn = Boolean(isLoggedIn)
 
 // console.log(boolIsLoggedIn);
+
+// 1 => true; 0 => false
+
+// "saiee" => true;"" => false
+
 
 
 let someNum = 33
