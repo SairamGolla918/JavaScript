619be7254fe1941c5356bf62943737d66e7e7aa0
SairamGolla918 <gollasairam918@gmail.com>
2024-11-12 18:51:37 +0530

    comparison Done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2 >= 1)
+// console.log(2 <= 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("02" > 2)
+// console.log("02" > 1)
+
+// console.log(null > 0 )
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+
+// --------- the above comparison should be avoided as the above opertors converts the datatype --------
+
+// ===
+
+console.log("2" == 2)
