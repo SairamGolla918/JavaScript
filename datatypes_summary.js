09bc75f825b6d7f87a8a0f30f2519f5ed29f3e35
SairamGolla918 <gollasairam918@gmail.com>
2024-11-12 21:34:40 +0530

    added datatypes

---
+++ datatypes_summary.js
@@ -0,0 +1,44 @@
+
+
+    //           Primitive 
+// Data types  < 
+    //           Reference 
+
+
+// Primitive Data Types
+
+// 7 types ( String , Number ,Boolean ,null ,undefined ,Symbol , BigInt)
+
+
+const score = 100 // - Number 
+const scoreValue = 100.3 // - Number
+
+const isLoggedIn = true
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log( id === anotherId)
+
+const bigNumber = 3547345567834565n
+
+
+// Refernce (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Saiee", "Yadav"] // Array
+let obj = 
+{
+    name:"Hai",
+    age:22
+}
+
+const myFun = function(){
+    console.log("Hello world")
+}
+
+console.log(typeof bigNumber)
