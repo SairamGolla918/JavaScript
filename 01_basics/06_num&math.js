0b280d079e94e43adde1f69070bfa5ba4b3cc5f1
SairamGolla918 <gollasairam918@gmail.com>
2024-11-14 13:05:07 +0530

    added num&Math

---
+++ 01_basics/06_num&math.js
@@ -0,0 +1,53 @@
+const num = 400
+
+const balance = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString())
+// console.log(balance.toFixed(1))
+
+
+// const otherNum = 209.000009
+
+// console.log(otherNum.toPrecision(5));
+
+// const hundreds = 100000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// --------------------Maths-----------------
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.555));
+
+// console.log(Math.ceil(4.1));
+
+// console.log(Math.floor(4.9))
+
+// console.log(Math.pow(2,10))
+
+// console.log(Math.max(1,2,3,5,5))
+
+// console.log(Math.min(1,2,34,-1,-2));
+
+console.log(Math.random());
+
+console.log(Math.random()*10 +1);
+
+
+console.log(Math.floor(Math.random()*10 + 1))
+
+
+const min = 10
+
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) ) + min);
+
+
