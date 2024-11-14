7982c55c22be5eeed940d62a2926dc7579b3819e
SairamGolla918 <gollasairam918@gmail.com>
2024-11-14 20:17:36 +0530

    learnt basics of array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,53 @@
+// Array
+
+const myArr = [0,1,2,3,4,5,"Saiee",20.000,true]
+
+
+const myNames =  ["saiee","yadav"]
+
+const an = new Array(1,2,3,4)
+
+// console.log(myArr[8]);
+
+
+
+
+// Array Methods
+
+// myArr.push(8)
+// myArr.push(9)
+// myArr.pop()
+// myArr.unshift(7)
+// myArr.shift()
+
+
+// console.log(myArr)
+// 
+// console.log(myArr.includes(9));
+
+const newArr = myArr.join()
+
+console.log(newArr);
+
+console.log(typeof newArr);
+
+// console.log(myArr.indexOf(9));
+
+console.log("A ",myArr);
+
+const myArr1 = myArr.slice(1,3)
+
+console.log(myArr1);
+
+const myArr2 = myArr.splice(1,3)
+console.log(myArr2);
+
+
+console.log("B ",myArr)
+
+
+
+
+
+
+
