405b4d375dd286eb55846d1fcfebc17c3ce02972
SairamGolla918 <gollasairam918@gmail.com>
2024-11-14 11:56:39 +0530

    added strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,44 @@
+const name = "Saiee"
+const repoCount = 10
+
+// console.log(name+repoCount+" Value")
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+
+const myname = new String("SaieeYadav")
+
+// console.log(myname[0]);
+// console.log(myname.__proto__)
+
+// console.log(myname.length);
+
+// console.log(myname.toUpperCase());
+
+// console.log(myname.charAt(2))
+
+// console.log(myname.split('e'))
+
+// console.log(myname.indexOf('i'))
+
+// console.log(myname.substring(0,5))
+
+// console.log(myname.slice(0,-9))
+
+// const sample = "    saiee"
+// console.log(sample.trim())
+
+const url = "https://sairamgolla.github.io%20Yadav"
+
+console.log(url.replace('%20','_'))
+
+console.log(url.includes('Yadav'))
+
+console.log(url.small())
+
+console.log(url.big());
+
+console.log(url.bold());
+
+
+
