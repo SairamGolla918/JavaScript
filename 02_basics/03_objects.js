bcd7ccb4fc03e8178a600954edbd5827a5ba066a
SairamGolla918 <gollasairam918@gmail.com>
2024-11-16 21:49:52 +0530

    Learnt Basics of Objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -16,11 +16,33 @@ const JsUser = {
     lastLogged :["Monaday","Friday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser["full name"]);
-console.log(typeof JsUser[mySym]);
-console.log();
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(typeof JsUser[mySym]);
+// console.log(JsUser[mySym]);
+// console.log(JsUser);
+
+JsUser.email = "codewithrowdy@gmail.com"
+
+// Object.freeze(JsUser)
+
+JsUser.email = "r180918@rguktrkv.ac.in"
+
+// console.log(JsUser);
+
+
+JsUser.greeting = function(){
+    console.log("Hello JS")
+}
+
+JsUser.greeting2 = function(){
+    console.log(`Hello JS,${this.name}`)
+}
+
+console.log(JsUser.greeting2())
+
+
 
 
 
