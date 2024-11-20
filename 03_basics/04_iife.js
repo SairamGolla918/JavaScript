0a8ad57f23959f47badabea5ded0fd51f6494c8d
SairamGolla918 <gollasairam918@gmail.com>
2024-11-20 14:52:36 +0530

    learnt IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,16 @@
+// Immediately Invoked Function Expressions(IIFE)
+
+
+(function Hai() {
+
+    // Named IIFE
+    console.log("Hai"); 
+})();
+
+
+
+( (NAME) => {
+    // Unnamed IIFE
+    console.log(`DB CONNECTED ${NAME}`);
+})('Saiee')
+
